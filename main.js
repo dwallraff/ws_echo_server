2202e974180f2f1ce72a1d12f69b8716f0307343
dwallraff <dave.wallraff@gmail.com>
2015-05-30 16:31:41 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var port = (process.env.VCAP_APP_PORT || 8192);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var http = require('http');
+var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: port});
+wss.on('connection', function(ws) {
+	ws.on('message', function(message) {
+		ws.send(message);
+		});
+	ws.send('something');
+});
